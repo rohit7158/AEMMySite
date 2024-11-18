311194158d808e5cc0a551221ae3fdf3cf634db4
Rohit Chauhan <rohit7158@hotmail.com>
2024-11-18 08:11:37 -0500

    Model Exporter

--- core/src/main/java/com/mysite/core/models/ArticleDetailsModel.java
+++ core/src/main/java/com/mysite/core/models/ArticleDetailsModel.java
@@ -2,15 +2,33 @@ package com.mysite.core.models;
 
 import java.util.Date;
 
+import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+import com.adobe.cq.export.json.ExporterConstants;
+import com.mysite.core.services.UserDataApiService;
+
+@Model(
+	adaptables = {Resource.class, SlingHttpServletRequest.class},
+	resourceType="mysite/components/article-detail",
+	defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Exporter(name=ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
 public class ArticleDetailsModel {
     
+	@OSGiService
+	UserDataApiService articleService;
+	
+	@SlingObject
+	ResourceResolver resolver;
+	
     @ValueMapValue(name = "articleTitle")
     @Default(values= "Article")
     private String title;
