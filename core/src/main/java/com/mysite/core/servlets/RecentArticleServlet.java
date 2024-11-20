ceb1aba3c782c77444c546975bce675cd92e1a5a
Rohit Chauhan <rohit7158@hotmail.com>
2024-11-19 19:14:55 -0500

    Path Based Servlet

---
+++ core/src/main/java/com/mysite/core/servlets/RecentArticleServlet.java
@@ -0,0 +1,44 @@
+package com.mysite.core.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.apache.sling.servlets.annotations.SlingServletPaths;
+import org.osgi.service.component.annotations.Component;
+
+@Component(service = Servlet.class)
+@SlingServletPaths(value = {"/bin/mysite/recent-article", "/bin/mysite/recent-articles"})
+public class RecentArticleServlet extends SlingAllMethodsServlet {
+
+    @Override
+    protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response)
+            throws ServletException, IOException {
+                response.getWriter().write("Inside doGet Method of Servlet");
+    }
+
+    
+    @Override
+    protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response)
+            throws ServletException, IOException {
+                response.getWriter().write("Inside doPost Method of Servlet");
+    }
+
+    
+    @Override
+    protected void doPut(SlingHttpServletRequest request, SlingHttpServletResponse response)
+            throws ServletException, IOException {
+                response.getWriter().write("Inside doPut Method of Servlet");
+    }
+
+    
+    @Override
+    protected void doDelete(SlingHttpServletRequest request, SlingHttpServletResponse response)
+            throws ServletException, IOException {
+                response.getWriter().write("Inside doDelete Method of Servlet");
+    }
+}
